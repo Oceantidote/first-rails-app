6b4031e2877bee4cceec4c9896cf2e5b5306c230
leonard percival <leonardpercival@gmail.com>
2021-02-15 09:52:27 +0000

    about page working

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  def about
+    @members = ['arthur', 'alex', 'celine', 'kat', 'lucien']
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # www.kitt.com/about
+  # verb 'path', to: 'contoller_name#method'
+  get 'about', to: 'pages#about'
 end
