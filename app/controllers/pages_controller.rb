cf8778ea241b7093c646223395423f796e9dc1bd
leonard percival <leonardpercival@gmail.com>
2021-02-15 10:33:03 +0000

    all done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,13 @@ class PagesController < ApplicationController
     @members = ['arthur', 'alex', 'celine', 'kat', 'lucien']
   end
 
+  def home
+  end
+
+  def contact
+    @members = ['arthur', 'alex', 'celine', 'kat', 'lucien']
+    if params[:member].present?
+      @member = @members.find { |member| member.start_with?(params[:member]) }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # www.kitt.com/about
+
+  # defines the naked url root i.e. www.kitt.com
+  root to: 'pages#home'
+  # get '/', to: 'pages#home'
+
   # verb 'path', to: 'contoller_name#method'
   get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact', as: 'email'
 end
